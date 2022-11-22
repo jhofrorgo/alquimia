7628f15727a79cffa2221277555a0e51be38dbe5
jhofrorgo <jhofrgo@gmail.com>
2022-11-22 00:07:03 -0500

    Menú y cabecera funcionales y separadas befr-login

---
+++ Vistas/modulos/cabecera.php
@@ -0,0 +1,38 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="http://localhost/alquimia/Inicio" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b class="fa fa-cutlery" style="font-size: 25px;"></b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b class="fa fa-cutlery" style="font-size: 25px;"> Alquimia </b>Soft </span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <span class="hidden-xs">Jhon Ortiz</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User profile -->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="http://localhost/alquimia/" class="btn btn-primary btn-flat">Mi perfil</a>
+                </div>
+                <div class="pull-right">
+                  <a href="http://localhost/alquimia/" class="btn btn-danger btn-flat">Cerrar Sesión</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
