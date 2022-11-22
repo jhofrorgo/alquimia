7e2f4dab015abe46158078c6c77bc35c4a6f0131
jhofrorgo <jhofrgo@gmail.com>
2022-11-22 00:17:32 -0500

    home limpio, se crea el archivo htcacces y conexbd

---
+++ Vistas/modulos/Ingresar.php
@@ -0,0 +1,37 @@
+<div class="login-box">
+  <div class="login-logo">
+    <h1>Alquimia Mixología Garrotera</h1>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Inicio de sesión</p>
+
+    <form method="post">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="usuario" placeholder="Usuario">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" name="clave" placeholder="Contraseña">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
+        </div>
+        <!-- /.col -->
+      </div>
+
+      <?php
+
+      $inicio = new UsuariosC();
+      $inicio -> IniciarSesionC();
+
+      ?>
+
+    </form>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
